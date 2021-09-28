543df172ddb8b4a41c0fcc09f53088c1df3e8508
Mansur Saleem <mansurmsaleem@gmail.com>
2021-09-28 12:34:54 -0400

    Updated About Me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -12,7 +12,7 @@ const AboutMe = () => {
 <div className="col-lg-6 col-xm-12">
     <h1  className="about-heading"> About Me </h1>
     <p>
-        Salutations, my name is Mansur and I am a junior web developer. I graduate from Woz-U on August 8th 2021. My program length was 9 months with a focus on Full-Stack web development. In a previous life, I was a IT technician for six years where I was very customer focused and always triaged issues with a smile. I bring the same level of problem solving to the development field, whether working on a website or a business application. I do not plan on sticking to just Web Applications, as I want to become a good Programmer, no matter what language I find myself working in.
+        Salutations, my name is Mansur and I am a junior web developer. I graduated from Woz-U on August 8th 2021. My program length was 9 months with a focus on Full-Stack web development. In a previous life, I was a IT technician for six years where I was very customer focused and always triaged issues with a smile. I bring the same level of problem solving to the development field, whether working on a website or a business application.
     </p>
     
 </div>
